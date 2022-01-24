2e6f287e9db802fdc77a996af7d54044e426d081
devang-selland <97459235+devang-selland@users.noreply.github.com>
2022-01-25 00:46:33 +0530

    Update functions.php

--- functions.php
+++ functions.php
@@ -1,12 +1,12 @@
 <?php
 /**
- * Open Web Office functions and definitions
+ * THEME NAME functions and definitions
  *
  * @link https://developer.wordpress.org/themes/basics/theme-functions/
  *
  * @package WordPress
- * @subpackage Open_Web_Office
- * @since Open Web Office 1.0
+ * @subpackage THEME_NAME
+ * @since THEME NAME 1.0
  */
 
 /**
@@ -21,9 +21,7 @@
  */
 $includes_files = array(
     'template-tags.php',
-	'svg-icons.php',  
-	'theme-functions.php',
-	'custom-theme-functions.php'  
+    'theme-functions.php',
 );
 
 foreach($includes_files as $filename){
@@ -36,20 +34,9 @@ foreach($includes_files as $filename){
  * Add required Class files.
  */
 $class_files = array(
-    'class-openweboffice-svg-icons.php',
-	'class-openweboffice-customize.php',
-	'class-openweboffice-separator-control.php',
-	'class-openweboffice-walker-page.php',
-	'class-openweboffice-walker-comment.php',    
-	'class-openweboffice-script-loader.php',
-	'class-openweboffice-non-latin-languages.php',
-	'class-openweboffice-acf-missing.php',
-	'class-openweboffice-acf-theme-functions.php',	
+'class-THEME-NAME-acf-theme-functions.php',	
+'class-THEME-NAME-custom-post-type-module.php',
 );
-if (class_exists('ACF')) {
-	$enalble_custom_post_type_module = get_field("enalble_custom_post_type_module","option");
-	array_push($class_files,"class-openweboffice-custom-post-type-module.php");	
-}
 foreach($class_files as $class_file){
     require_once 'classes/' . $class_file ;
 }
